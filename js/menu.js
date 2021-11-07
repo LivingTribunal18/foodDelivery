a368eb5bd7ba846b7a55eb7e84bf470e38ab7ec6
karineB <karinebondarenko03@gmail.com>
2021-11-07 14:34:24 +0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,62 @@
+const cardsMenu = document.querySelector(".cards-menu");
+
+const restaurant = JSON.parse(localStorage.restaurant);
+
+const changeTitle = (restaurant) => {
+  document.querySelector(".restaurant-title").textContent = restaurant.name;
+  document.querySelector(".rating").textContent = restaurant.stars;
+  document.querySelector(".price").textContent = `От ${restaurant.price} ₽`;
+  document.querySelector(".category").textContent = restaurant.kitchen;
+};
+
 const renderItems = (items) => {
-  items.map((item) => {
-    console.log(item);
+  items.map(({ id, description, name, price, image }) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `
+        <img
+          src="${image}"
+          alt="${image}"
+          class="card-image"
+        />
+        <div class="card-text">
+          <div class="card-heading">
+            <h3 class="card-title card-title-reg">${name}</h3>
+          </div>
+          <div class="card-info">
+            <div class="ingredients">
+             ${description}
+            </div>
+          </div>
+          <div class="card-buttons">
+            <button class="button button-primary button-add-cart">
+              <span class="button-card-text">В корзину</span>
+              <span class="button-cart-svg"></span>
+            </button>
+            <strong class="card-price-bold">${price} ₽</strong>
+          </div>
+        </div>
+    `;
+
+    cardsMenu.append(div);
   });
 };
 
-async function fetchPertners() {
+async function fetchMenu() {
   try {
     const response = await axios.get(
-      "https://delivery-food-79d68-default-rtdb.europe-west1.firebasedatabase.app/"
+      `https://delivery-food-79d68-default-rtdb.europe-west1.firebasedatabase.app/db/${restaurant.products}`
     );
-    console.log(response.data);
     renderItems(response.data);
   } catch (e) {
     console.log(e);
   }
 }
 
-fetchPertners();
+if (restaurant) {
+  fetchMenu();
+  changeTitle(restaurant);
+} else {
+  window.location.href = "/index.html";
+}
--- js/partners.js
+++ js/partners.js
@@ -1,19 +1,60 @@
+const cardsRestaurants = document.querySelector(".cards-restaurants");
+const auth = document.querySelector(".button-auth");
+
 const renderItems = (items) => {
   items.map((item) => {
-    console.log(item);
+    const { image, kitchen, name, price, products, stars, time_of_delivery } =
+      item;
+
+    const a = document.createElement("a");
+    a.setAttribute("href", "/restaurant.html");
+    a.classList.add("card");
+    a.classList.add("card-restaurant");
+    a.dataset.products = products;
+    a.innerHTML = `
+      <img
+        src="${image}"
+        alt="${image}"
+        class="card-image"
+      />
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title">${name}</h3>
+          <span class="card-tag tag">${time_of_delivery} мин</span>
+        </div>
+        <div class="card-info">
+          <div class="rating"> ${stars}</div>
+          <div class="price">От ${price} ₽</div>
+          <div class="category">${kitchen}</div>
+        </div>
+      </div>
+    `;
+
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (localStorage.user) {
+        localStorage.setItem("restaurant", JSON.stringify(item));
+        window.location.href = "/restaurant.html";
+      } else {
+        let event = new Event("click");
+        auth.dispatchEvent(event);
+      }
+    });
+
+    cardsRestaurants.append(a);
   });
 };
 
-async function fetchPertners() {
+async function fetchPartners() {
   try {
     const response = await axios.get(
       "https://delivery-food-79d68-default-rtdb.europe-west1.firebasedatabase.app/db/partners.json"
     );
-    console.log(response.data);
     renderItems(response.data);
   } catch (e) {
     console.log(e);
   }
 }
 
-fetchPertners();
+fetchPartners();
